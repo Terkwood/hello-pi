4c2cc2a6c776e983836a86f7bd655f70d7d55f5a
Terkwood <38859656+Terkwood@users.noreply.github.com>
2018-06-12 19:15:47 -0400

    Add rust examples (#2)
    Adds examples in rust

--- py/flashy.py
+++ py/flashy.py
@@ -5,26 +5,26 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 blue_flag = int('001', 2)
-green_flag = int('010', 2)
+yellow_flag = int('010', 2)
 red_flag = int('100', 2)
 
 blue_pin = 17
-green_pin = 5
+yellow_pin = 5
 red_pin = 26
 
 GPIO.setup(blue_pin, GPIO.OUT)
-GPIO.setup(green_pin, GPIO.OUT)
+GPIO.setup(yellow_pin, GPIO.OUT)
 GPIO.setup(red_pin, GPIO.OUT)
 
 slow = 1.0
 fast = 0.03125
 duration = slow 
 
-counter = 0
+count = 0
 
 while True:
-    counter = counter + 1 % 8
-    GPIO.output(blue_pin, counter & blue_flag > 0)
-    GPIO.output(green_pin, counter & green_flag > 0)
-    GPIO.output(red_pin, counter & red_flag > 0)
+    count = (count + 1) % 8
+    GPIO.output(blue_pin, count & blue_flag > 0)
+    GPIO.output(yellow_pin, count & yellow_flag > 0)
+    GPIO.output(red_pin, count & red_flag > 0)
     time.sleep(duration)
---
+++ rs/src/lib.rs
@@ -0,0 +1,1 @@
+extern crate sysfs_gpio;
