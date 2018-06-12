6c4664529c66420dfb74fb726299c87d772c564f
Terkwood <>
2018-06-12 18:50:59 -0400

    fmt

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,8 +1,8 @@
 extern crate sysfs_gpio;
 
-use sysfs_gpio::{Direction, Pin};
 use std::thread::sleep;
 use std::time::Duration;
+use sysfs_gpio::{Direction, Pin};
 
 const BLUE_PIN: u64 = 17;
 const YELLOW_PIN: u64 = 5;
@@ -14,14 +14,14 @@ fn main() {
 
 fn blink() {
     let blue_pin = Pin::new(BLUE_PIN);
-    blue_pin.with_exported(|| {
-        loop {
+    blue_pin
+        .with_exported(|| loop {
             blue_pin.set_value(0).unwrap();
             sleep(Duration::from_millis(200));
             blue_pin.set_value(1).unwrap();
             sleep(Duration::from_millis(200));
-        }
-    }).unwrap();
+        })
+        .unwrap();
 }
 
 fn blink3() {
@@ -30,20 +30,20 @@ fn blink3() {
     let red_pin = Pin::new(RED_PIN);
 
     let duration = 125;
-    blue_pin.with_exported(|| {
-        yellow_pin.with_exported(|| {
-            red_pin.with_exported(|| {
-loop {
-                blue_pin.set_value(1).unwrap();
-                yellow_pin.set_value(1).unwrap();
-                red_pin.set_value(1).unwrap();
-                sleep(Duration::from_millis(duration));
-                blue_pin.set_value(0).unwrap();
-                yellow_pin.set_value(0).unwrap();
-                red_pin.set_value(0).unwrap();
-                sleep(Duration::from_millis(duration));
-            }
+    blue_pin
+        .with_exported(|| {
+            yellow_pin.with_exported(|| {
+                red_pin.with_exported(|| loop {
+                    blue_pin.set_value(1).unwrap();
+                    yellow_pin.set_value(1).unwrap();
+                    red_pin.set_value(1).unwrap();
+                    sleep(Duration::from_millis(duration));
+                    blue_pin.set_value(0).unwrap();
+                    yellow_pin.set_value(0).unwrap();
+                    red_pin.set_value(0).unwrap();
+                    sleep(Duration::from_millis(duration));
+                })
             })
         })
-    }).unwrap();
+        .unwrap();
 }
