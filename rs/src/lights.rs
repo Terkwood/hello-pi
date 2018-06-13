054f4057c84b4130ee1de42f53e98e306dc04010
Terkwood <Terkwood metaterkhorn@gmail.com>
2018-06-13 10:20:30 -0400

    err all

--- rs/src/lights.rs
+++ rs/src/lights.rs
@@ -63,10 +63,12 @@ pub fn flashy() {
                 if let Err(_e) = blue_pin.set_value((count & blue_flag > 0) as u8) {
                     println!("Couldn't set blue");
                 };
-                yellow_pin
-                    .set_value((count & yellow_flag > 0) as u8)
-                    .unwrap();
-                red_pin.set_value((count & red_flag > 0) as u8).unwrap();
+                if let Err(_e) = yellow_pin.set_value((count & yellow_flag > 0) as u8) {
+                    println!("Couldn't set yellow")
+                };
+                if let Err(_e) = red_pin.set_value((count & red_flag > 0) as u8) {
+                    println!("Couldn't set red")
+                };
                 sleep(Duration::from_millis(duration));
             })
         })
