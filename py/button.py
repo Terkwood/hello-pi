6a4ff8fd2b0599157edd4f54b6114f99a999ab3f
Terkwood <metaterkhorn@gmail.com>
2018-06-14 14:37:01 -0900

    BOOM - BUTTON

--- py/button.py
+++ py/button.py
@@ -1,12 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
-button_pin = 26 
+led_pin = 18
+button_pin = 25 
 
 GPIO.setwarnings(True)
-GPIO.setmode(GPIO.BCM) # use GPIO pin numbers
-GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Important: set initial value for pin low 
+GPIO.setmode(GPIO.BCM)   # use GPIO pin numbers
+GPIO.setup(led_pin, GPIO.OUT)  
+GPIO.setup(button_pin, GPIO.IN)
 
 while True:
-    if GPIO.input(button_pin) == GPIO.HIGH:
-        print("CLIMBING NOW!")
+    if GPIO.input(button_pin):
+        GPIO.output(led_pin, True)
+    else:
+        GPIO.output(led_pin, False)
