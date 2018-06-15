52931e3eb193b87404698d229a70686da0845005
Terkwood <metaterkhorn@gmail.com>
2018-06-15 12:01:08 -0400

    sleep heuristic

--- rs/src/button.rs
+++ rs/src/button.rs
@@ -5,12 +5,14 @@ use std::time::Duration;
 use sysfs_gpio::{Direction, Pin};
 
 pub fn run() {
+    const SLEEP_HEURISTIC_MILLIS: u64 = 1;
+
     let led_pin = Pin::new(18);
     let button_pin = Pin::new(25);
     led_pin
         .with_exported(|| {
             button_pin.with_exported(|| {
-                sleep(Duration::from_millis(200));
+                sleep(Duration::from_millis(SLEEP_HEURISTIC_MILLIS));
                 led_pin.set_direction(Direction::Out).unwrap();
                 button_pin.set_direction(Direction::In).unwrap();
                 loop {
