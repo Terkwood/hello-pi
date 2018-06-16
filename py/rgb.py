e9b465adff6bf50056ea1652e6fd752913b6186b
Terkwood <metaterkhorn@gmail.com>
2018-06-15 16:57:38 -0900

    fix things

--- py/rgb.py
+++ py/rgb.py
@@ -11,25 +11,29 @@ blue_led = PWMLED(20)
 
 button = Button(25)
 
-rand_mode = True
+rand_mode = False
 
 def reroll():
     change_color(rgb_rand(), rgb_rand(), rgb_rand())
     
 def pulse():
+    red_led.off()
     red_led.pulse()
+    green_led.off()
     green_led.pulse()
+    blue_led.off()
     blue_led.pulse()
-
 def state_change():
     global rand_mode
     rand_mode = not rand_mode
+    print(rand_mode)
     if rand_mode:
         reroll()
     else:
+        time.sleep(0.1)
         pulse()
 
-button.when_pressed = reroll
+button.when_pressed = state_change
 
 def bounded(n):
     return min(max(n, 0.0), 1.0)
@@ -43,7 +47,9 @@ def change_color(r, g, b):
 def rgb_rand():
     return random.uniform(0.0, 0.5)
 
-reroll()
+pulse()
 
 while True:
-    True
+    if rand_mode:
+        time.sleep(0.1)
+        reroll()
