7c3af97b940c80bb76df690d43d8509624f264a5
Terkwood <metaterkhorn@gmail.com>
2018-06-20 13:06:50 -0400

    rppal example

--- pi_service/src/main.rs
+++ pi_service/src/main.rs
@@ -1,3 +1,23 @@
+extern crate rppal;
+
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::{Gpio, Mode, Level};
+use rppal::system::DeviceInfo;
+
+// The GPIO module uses BCM pin numbering. BCM GPIO 18 is located on physical pin 12.
+const GPIO_LED: u8 = 12;
+
 fn main() {
-    println!("Hello, world!");
+    let device_info = DeviceInfo::new().unwrap();
+    println!("Model: {} (SoC: {})", device_info.model(), device_info.soc());
+
+    let mut gpio = Gpio::new().unwrap();
+    gpio.set_mode(GPIO_LED, Mode::Output);
+
+    // Blink an LED attached to the pin on and off
+    gpio.write(GPIO_LED, Level::High);
+    thread::sleep(Duration::from_millis(500));
+    gpio.write(GPIO_LED, Level::Low);
 }
