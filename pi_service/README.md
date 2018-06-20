671754ea3d62243d39e4ba3a9eefb644efafd401
Terkwood <metaterkhorn@gmail.com>
2018-06-20 14:00:27 -0400

    this

--- pi_service/README.md
+++ pi_service/README.md
@@ -6,8 +6,13 @@ A service which executes instructions against Raspberry Pi based on messages rec
 
 Always use caution when wiring Raspberry Pi, and especially when executing arbitrary scripts that you find on the internet.  This project won't work with your Raspberry Pi wiring unless you've taken extreme care to match the configuration shown here.  This project is intended as an educational example only :fire:
 
+## Acknowledgements
+
+[rust-wiringpi](https://github.com/Ogeon/rust-wiringpi/blob/master/src/bindings.rs) was extremely helpful here.  Thank you.
+
 ## References
 
+* http://wiringpi.com/
+* https://github.com/Ogeon/rust-wiringpi/blob/master/src/bindings.rs
 * https://www.admfactory.com/breathing-light-led-on-raspberry-pi-using-c/
 * http://www.bristolwatch.com/rpi/pwmRpi.htm
-* 
--- pi_service/src/main.rs
+++ pi_service/src/main.rs
@@ -1,8 +1,8 @@
 extern crate wiringpi;
 
-use wiringpi::pin::Value::High;
-use std::time::Duration;
 use std::thread;
+use std::time::Duration;
+use wiringpi::pin::Value::High;
 
 fn main() {
     // Setup wiringPi in GPIO mode (with original BCM numbering order)
@@ -10,22 +10,20 @@ fn main() {
 
     let mut alice = pi.soft_pwm_pin(12);
 
-    // Use a duty cycle of 0.5 on both pins
-    alice.pwm_write(50);
+    loop {
+        // Duty cycle ranges from 0 to 100
+        for i in 0..101 {
+            alice.pwm_write(i);
+            thread::sleep(Duration::from_millis(1));
+        }
 
-    thread::sleep(Duration::from_millis(2000));
+        thread::sleep(Duration::from_millis(10));
 
-    let alice_out = alice.into_output();
-    alice_out.digital_write(High);
+        for i in 0..101 {
+            alice.pwm_write(100 - i);
+            thread::sleep(Duration::from_millis(1));
+        }
 
-    thread::sleep(Duration::from_millis(2000));
-
-    // Switch `alice_out` (pin 12) back to software PWM mode
-    alice = alice_out.into_soft_pwm();
-    alice.pwm_write(1024);
-
-    thread::sleep(Duration::from_millis(2000));
-
-    alice.pwm_write(0);
-
+        thread::sleep(Duration::from_millis(10));
+    }
 }
