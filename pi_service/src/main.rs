5086c7827acf07a968bb42ad50f625105e2b625d
Terkwood <metaterkhorn@gmail.com>
2018-06-20 13:37:09 -0400

    try

--- pi_service/src/main.rs
+++ pi_service/src/main.rs
@@ -1,24 +1,31 @@
 extern crate wiringpi;
 
-use wiringpi::pin::Value::{High, Low};
-use std::{thread, time};
+use wiringpi::pin::Value::High;
+use std::time::Duration;
+use std::thread;
 
 fn main() {
-    //Setup WiringPi using GPIO pin numbering
+    // Setup wiringPi in GPIO mode (with original BCM numbering order)
     let pi = wiringpi::setup_gpio();
 
-    //Use WiringPi pin 12 as output
-    let pin = pi.output_pin(12);
+    let mut alice = pi.soft_pwm_pin(12);
 
-    let interval = time::Duration::from_millis(1000);
+    // Use a duty cycle of 0.5 on both pins
+    alice.pwm_write(50);
 
-    loop {
-        //Set pin 0 to high and wait one second
-        pin.digital_write(High);
-        thread::sleep(interval);
+    thread::sleep(Duration::from_millis(2000));
 
-        //Set pin 0 to low and wait one second
-        pin.digital_write(Low);
-        thread::sleep(interval);
-    }
+    let alice_out = alice.into_output();
+    alice_out.digital_write(High);
+
+    thread::sleep(Duration::from_millis(2000));
+
+    // Switch `alice_out` (pin 12) back to software PWM mode
+    alice = alice_out.into_soft_pwm();
+    alice.pwm_write(50);
+
+    thread::sleep(Duration::from_millis(2000));
+
+    alice.pwm_write(0);
+
 }
