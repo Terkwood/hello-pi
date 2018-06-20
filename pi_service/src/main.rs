9ae2c9944c0c883dbbe07152005ba482af883c16
Terkwood <metaterkhorn@gmail.com>
2018-06-20 14:06:39 -0400

    try

--- pi_service/src/main.rs
+++ pi_service/src/main.rs
@@ -8,19 +8,23 @@ fn main() {
     // Setup wiringPi in GPIO mode (with original BCM numbering order)
     let pi = wiringpi::setup_gpio();
 
-    let mut alice = pi.soft_pwm_pin(12);
+    let red_led = pi.soft_pwm_pin(12);
+    let green_led = pi.soft_pwm_pin(16);
+    let blue_led = pi.soft_pwm_pin(20);
 
     loop {
         // Duty cycle ranges from 0 to 100
-        for i in 0..101 {
-            alice.pwm_write(i);
+        for i in 0..256 {
+            let v = i as f32 / 255.0;
+            red_led.pwm_write(v as i32);
             thread::sleep(Duration::from_millis(1));
         }
 
         thread::sleep(Duration::from_millis(10));
 
-        for i in 0..101 {
-            alice.pwm_write(100 - i);
+        for i in 0..256 {
+            let v = i as f32 / 255.0;
+            red_led.pwm_write(100 - i);
             thread::sleep(Duration::from_millis(1));
         }
 
