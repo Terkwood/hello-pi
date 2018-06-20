69fb841fed7b2dba1af267c9a690deead5c8ad9a
Terkwood <metaterkhorn@gmail.com>
2018-06-20 16:19:33 -0400

    try a channel

--- pi_service/src/main.rs
+++ pi_service/src/main.rs
@@ -2,53 +2,109 @@
 extern crate crossbeam_channel as channel;
 extern crate wiringpi;
 
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
 enum PiOutput {
-    Led { pin: u32, value: u32 },
-    Blink { pin: u32, millis: u32 },
+    Led { pin: u16, value: i32 },
+    Blink { pin: u16, millis: u32 },
+}
+
+const RED_GPIO: u16 = 12;
+const GREEN_GPIO: u16 = 16;
+const BLUE_GPIO: u16 = 20;
+
+fn pi_consumer(output_r: channel::Receiver<PiOutput>) {
+    // Setup wiringPi in GPIO mode (with original BCM numbering order)
+    let pi = wiringpi::setup_gpio();
+    let pins = {
+        let mut p = HashMap::new();
+
+        // track some pins
+        p.insert(RED_GPIO, pi.soft_pwm_pin(RED_GPIO)); // red
+        p.insert(GREEN_GPIO, pi.soft_pwm_pin(GREEN_GPIO)); // green
+        p.insert(BLUE_GPIO, pi.soft_pwm_pin(BLUE_GPIO)); // blue
+        p
+    };
+    loop {
+        match output_r.recv() {
+            Some(PiOutput::Led { pin, value }) => if let Some(p) = pins.get(&pin) {
+                p.pwm_write(value);
+            },
+            Some(PiOutput::Blink { pin: _, millis: _ }) => {}
+            None => {}
+        }
+    }
 }
 
 fn main() {
-    //let (output_s, output_r) = channel::bounded(1);
+    let (output_s, output_r) = channel::bounded(1);
 
-    // Setup wiringPi in GPIO mode (with original BCM numbering order)
-    let pi = wiringpi::setup_gpio();
-
-    let red_led = pi.soft_pwm_pin(12);
-    let green_led = pi.soft_pwm_pin(16);
-    let blue_led = pi.soft_pwm_pin(20);
+    thread::spawn(move || pi_consumer(output_r));
 
     // clear
-    red_led.pwm_write(0);
-    green_led.pwm_write(0);
-    blue_led.pwm_write(0);
+    output_s.send(PiOutput::Led {
+        pin: RED_GPIO,
+        value: 0,
+    });
+    output_s.send(PiOutput::Led {
+        pin: GREEN_GPIO,
+        value: 0,
+    });
+    output_s.send(PiOutput::Led {
+        pin: BLUE_GPIO,
+        value: 0,
+    });
 
     // show a pleasant green
-    red_led.pwm_write(duty_value(47));
-    green_led.pwm_write(duty_value(181));
-    blue_led.pwm_write(duty_value(47));
+    output_s.send(PiOutput::Led {
+        pin: RED_GPIO,
+        value: from_color(47),
+    });
+    output_s.send(PiOutput::Led {
+        pin: GREEN_GPIO,
+        value: from_color(181),
+    });
+    output_s.send(PiOutput::Led {
+        pin: BLUE_GPIO,
+        value: from_color(47),
+    });
 
     thread::sleep(Duration::from_secs(5));
 
     // clear green
-    green_led.pwm_write(0);
+    output_s.send(PiOutput::Led {
+        pin: GREEN_GPIO,
+        value: 0,
+    });
 
     loop {
         for i in 0..256 {
-            let v = duty_value(i);
-            red_led.pwm_write(v);
-            blue_led.pwm_write(v);
+            let v = from_color(i);
+            output_s.send(PiOutput::Led {
+                pin: RED_GPIO,
+                value: v,
+            });
+            output_s.send(PiOutput::Led {
+                pin: BLUE_GPIO,
+                value: v,
+            });
             thread::sleep(Duration::from_millis(1));
         }
 
         thread::sleep(Duration::from_millis(10));
 
         for i in 0..256 {
-            let v = 100 - duty_value(i);
-            red_led.pwm_write(v);
-            blue_led.pwm_write(v);
+            let v = 100 - from_color(i);
+            output_s.send(PiOutput::Led {
+                pin: RED_GPIO,
+                value: v,
+            });
+            output_s.send(PiOutput::Led {
+                pin: BLUE_GPIO,
+                value: v,
+            });
             thread::sleep(Duration::from_millis(1));
         }
 
@@ -56,7 +112,7 @@ fn main() {
     }
 
     // Duty cycle ranges from 0 to 100
-    fn duty_value(color: i32) -> i32 {
+    fn from_color(color: i32) -> i32 {
         (color as f32 / 255.0 * 100.0) as i32
     }
 }
