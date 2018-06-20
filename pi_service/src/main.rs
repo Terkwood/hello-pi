c3452258f1d4d0cccc5984fd213895d890a8bd64
Terkwood <metaterkhorn@gmail.com>
2018-06-20 14:22:21 -0400

    rename

--- pi_service/src/main.rs
+++ pi_service/src/main.rs
@@ -11,23 +11,25 @@ fn main() {
     let green_led = pi.soft_pwm_pin(16);
     let blue_led = pi.soft_pwm_pin(20);
 
+    // clear
     red_led.pwm_write(0);
     green_led.pwm_write(0);
     blue_led.pwm_write(0);
 
     // show a pleasant green
-    red_led.pwm_write(pwm_value(47));
-    green_led.pwm_write(pwm_value(181));
-    blue_led.pwm_write(pwm_value(47));
+    red_led.pwm_write(duty_value(47));
+    green_led.pwm_write(duty_value(181));
+    blue_led.pwm_write(duty_value(47));
 
     thread::sleep(Duration::from_secs(5));
 
+    // clear green
     green_led.pwm_write(0);
 
     loop {
         // Duty cycle ranges from 0 to 100
         for i in 0..256 {
-            let v = pwm_value(i);
+            let v = duty_value(i);
             red_led.pwm_write(v);
             blue_led.pwm_write(v);
             thread::sleep(Duration::from_millis(1));
@@ -36,7 +38,7 @@ fn main() {
         thread::sleep(Duration::from_millis(10));
 
         for i in 0..256 {
-            let v = 100 - pwm_value(i);
+            let v = 100 - duty_value(i);
             red_led.pwm_write(v);
             blue_led.pwm_write(v);
             thread::sleep(Duration::from_millis(1));
@@ -45,7 +47,7 @@ fn main() {
         thread::sleep(Duration::from_millis(10));
     }
 
-    fn pwm_value(color_value: i32) -> i32 {
-        (color_value as f32 / 255.0 * 100.0) as i32
+    fn duty_value(color: i32) -> i32 {
+        (color as f32 / 255.0 * 100.0) as i32
     }
 }
