ba4ab5f4789b59216abdab3c740a2b862f7ff7f9
Terkwood <metaterkhorn@gmail.com>
2018-06-20 21:18:33 -0400

    park

--- pi_service/src/main.rs
+++ pi_service/src/main.rs
@@ -18,6 +18,7 @@ fn main() {
     thread::spawn(move || gpio_receiver::run(gpio_r));
     thread::spawn(move || redis_subscribe::run(gpio_s));
 
+    thread::park();
     // Duty cycle ranges from 0 to 100
     fn from_color(color: i32) -> i32 {
         (color as f32 / 255.0 * 100.0) as i32
