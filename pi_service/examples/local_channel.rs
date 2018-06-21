4c1bacdb4e0c9b4d8d7dda6238075d5aa0b8ff88
Terkwood <metaterkhorn@gmail.com>
2018-06-20 20:17:58 -0400

    mod

--- pi_service/examples/local_channel.rs
+++ pi_service/examples/local_channel.rs
@@ -2,6 +2,11 @@ extern crate crossbeam_channel as channel;
 extern crate redis;
 extern crate wiringpi;
 
+mod model;
+mod pi_receiver;
+mod pins;
+mod redis_subscribe;
+
 use model::WritePwm;
 use pins::*;
 use std::thread;
