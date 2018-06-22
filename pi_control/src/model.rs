f06258cd785a2470d75bd725f7f003e11f0ef82f
Terkwood <metaterkhorn@gmail.com>
2018-06-21 21:06:07 -0400

    fiddle with license

--- pi_control/src/model.rs
+++ pi_control/src/model.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2018 Terkwood
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 /// Represents the state of an RGB widget
 /// color is an array of values from 0.0 to 1.0
 pub struct SetRGB {
