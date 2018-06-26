8d6b4dd2dbbd18c425c83746321894a1be266ec0
Terkwood <38859656+Terkwood@users.noreply.github.com>
2018-06-26 00:35:47 -0400

    Add MIDI light show (#15)
    https://www.youtube.com/watch?v=9y1bVPCvIWE

---
+++ midi_light_show/README.md
@@ -0,0 +1,95 @@
+# midi_light_show
+
+This project implements a simple LED light and music show which can be executed on a Raspberry Pi.
+
+## Raspberry Pi LED Setup and Wiring
+
+We used the following wiring setup on a Raspberry Pi 3 B+.  Be careful to check the GPIO pins of your specific model of Pi, as they may not match what is shown below.
+
+![Fritzing Diagram](doc/midi_light_show.jpg)
+
+## Implementation
+
+Uses [rtmidi](https://github.com/thestk/rtmidi), [rimd](https://github.com/RustAudio/rimd) and [midir](https://github.com/Boddlnagg/midir) libs to read MIDI and output it to an audio device.
+
+## Usage
+
+We recommend downloading the JS Bach Goldberg Variations from https://www.opengoldbergvariations.org/.
+
+```sh
+cargo run MIDI_FILE MIDI_DEVICE_ID
+```
+
+For example:
+
+```sh
+cargo run ~/Documents/Goldberg_Variations.mid 1
+```
+
+## Building on Raspbian
+
+You need to install `libsound2`.
+
+```sh
+sudo apt-get install libasound2-dev
+```
+
+At this point you can then build the `alsa` crate for rust on your Pi.
+
+## Misc OS Configs
+
+### Mac OS X
+
+* Install SimpleSynth according to https://github.com/wbsoft/frescobaldi/wiki/MIDI-playback-on-Mac-OS-X
+
+### Raspbian
+
+### Virtual MIDI
+
+Required reading: http://sandsoftwaresound.net/qsynth-fluidsynth-raspberry-pi/
+
+Key insight:  you need to start `fluidsynth` as a server, allow MIDI input, connect it to your ALSA sound device on the Pi, and then correctly select the device number when starting this application:
+
+```sh
+fluidsynth -a alsa -i /usr/share/sounds/sf2/FluidR3_GM.sf2 --server
+aconnect -lio   #  and COUNT the number of MIDI related devices
+cargo run ~/Goldberg_Variations.mid 1  # if fluidsynth is the only virtual midi device on your pi, it should have ID 1.  see `aconnect -lio`
+```
+
+Helper command -- play a midi file to your speaker using `fluidsynth`:
+
+```sh
+fluidsynth -a alsa -n -i /usr/share/sounds/sf2/FluidR3_GM.sf ~/Goldberg_Variations.mid
+```
+
+See MIDI devices:
+
+```sh
+aconnect -lio
+```
+
+See sound cards:
+
+```sh
+aplay -l
+```
+
+### Output to HDMI monitor
+
+If you're trying to use an HDMI monitor as an ALSA device in Raspbian, see https://raspberrypi.stackexchange.com/questions/4289/how-to-make-raspberry-use-hdmi-audio-as-default-system-output
+
+You'll need to uncomment `hdmi_drive=2` in `/boot/config.txt`.
+
+## Acknowledgements
+
+Bach's Goldberg Variations are [available under Creative Commons License here](https://www.opengoldbergvariations.org/).
+
+The availability of the following audio libraries is greatly appreciated:
+
+* Big thanks to [midir library](https://github.com/Boddlnagg/midir).
+* Big thanks to [rimd library](https://github.com/RustAudio/rimd).
+* Big thanks to [rtmidi library](https://github.com/thestk/rtmidi).
+
+Thank you to [fluidsynth](http://www.fluidsynth.org/), which allowed us to send MIDI output to an audio device on Raspberry Pi.
+
+We recommend listening to [Fredrik Johansson's MIDI repository](https://github.com/fredrik-johansson/midi), an excellent, extensive body of work!  Bravo!
---
+++ midi_light_show/src/light.rs
@@ -0,0 +1,126 @@
+//MIT License
+
+//Copyright (c) 2018 Terkwood
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+extern crate crossbeam_channel as channel;
+extern crate wiringpi;
+
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+use std::collections::HashMap;
+use ChannelEvent::{ChannelOff, ChannelOn};
+use MidiNoteEvent;
+
+static PINS: &'static [u16; 8] = &[13, 6, 5, 7, 23, 18, 15, 14];
+
+pub fn run(output_r: channel::Receiver<MidiNoteEvent>) {
+    // Setup wiringPi in GPIO mode (with original BCM numbering order)
+    let pi = wiringpi::setup_gpio();
+
+    let led_pin_outs = {
+        let mut lpos = Vec::new();
+        // track some pins
+        for &p in PINS {
+            lpos.push(pi.output_pin(p));
+        }
+
+        lpos
+    };
+    // key: index from 0..8 corresponding to the physical order of the LEDs
+    // value: MIDI channel
+    let mut led_to_midi_channel: HashMap<usize, u8> = HashMap::new();
+
+    loop {
+        match output_r.recv() {
+            Some(MidiNoteEvent {
+                channel_event: ChannelOff(c),
+                time: _,
+                vtime: _,
+                note: _,
+                velocity: _,
+            })
+            | Some(MidiNoteEvent {
+                channel_event: ChannelOn(c),
+                time: _,
+                vtime: _,
+                note: _,
+                velocity: 0,
+            }) => {
+                let mut unset: Vec<usize> = vec![];
+                for (led, lchan) in &led_to_midi_channel {
+                    if c == *lchan {
+                        // turn off the LED
+                        led_pin_outs[*led].digital_write(wiringpi::pin::Value::Low);
+                        unset.push(*led);
+                        println!("LOW  current on LED #{} (pin {})", *led, PINS[*led]);
+                    }
+                }
+                for u in unset {
+                    led_to_midi_channel.remove(&u);
+                }
+            }
+            Some(MidiNoteEvent {
+                channel_event: ChannelOn(c),
+                time: _,
+                vtime: _,
+                note,
+                velocity: _,
+            }) => {
+                let led = midi_note_to_led(note);
+                // only mess with this note if it's
+                // not being used by another channel
+                match led_to_midi_channel.entry(led) {
+                    Vacant(entry) => {
+                        led_pin_outs[led].digital_write(wiringpi::pin::Value::High);
+                        entry.insert(c);
+                        println!("HIGH current on LED #{} (pin {})", led, PINS[led]);
+                    }
+                    Occupied(_entry) => (),
+                }
+            }
+            None => {}
+        }
+    }
+}
+
+fn midi_note_to_led(c: u8) -> usize {
+    const NUM_LEDS: i8 = 8;
+    ((60 - c as i8).modulo(NUM_LEDS)) as usize
+}
+
+///
+/// Modulo that handles negative numbers, works the same as Python's `%`.
+///
+/// eg: `(a + b).modulo(c)`
+/// from https://stackoverflow.com/questions/31210357/is-there-a-modulus-not-remainder-function-operation
+pub trait ModuloSignedExt {
+    fn modulo(&self, n: Self) -> Self;
+}
+macro_rules! modulo_signed_ext_impl {
+    ($($t:ty)*) => ($(
+        impl ModuloSignedExt for $t {
+            #[inline]
+            fn modulo(&self, n: Self) -> Self {
+                (self % n + n) % n
+            }
+        }
+    )*)
+}
+modulo_signed_ext_impl! { i8 i16 i32 i64 }
