9e87808bd6ce3b6d92d4f97d1a8bdadcdf460908
Terkwood <metaterkhorn@gmail.com>
2018-07-02 18:55:09 -0400

    try 1 1

--- midi_light_show/src/main.rs
+++ midi_light_show/src/main.rs
@@ -113,7 +113,7 @@ impl MidiTimeInfo {
 
     pub fn micros_per_clock(self: &Self) -> u64 {
         const MAGIC0: f32 = 24.0;
-        const MAGIC1: f32 = 4.0;
+        const MAGIC1: f32 = 1.0;
         // SO, THIS IS A ROUGH ESTIMATE
         // ...and if `num_32nd_notes_per_24_ticks` is set in your MIDI file,
         // ...you should do more arithmetic.
