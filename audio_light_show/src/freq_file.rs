b87cb39e747ddc5d0f568182090e3d6c8b2b117a
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-24 12:00:25 -0400

    Wire up loading aubiopitch file

---
+++ audio_light_show/src/freq_file.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+use crate::TimeFreq;
+
+#[derive(Debug)]
+pub struct LoadErr;
+
+pub fn load<P>(filename: P) -> Result<Vec<TimeFreq>, LoadErr>
+where
+    P: AsRef<Path>,
+{
+    if let Ok(lines) = read_lines(filename) {
+        let mut out = vec![];
+        for line in lines {
+            if let Ok(l) = line {
+                if let Ok(tf) = parse(&format!("{}", l)) {
+                    out.push(tf);
+                }
+            }
+        }
+        Ok(out)
+    } else {
+        Err(LoadErr)
+    }
+}
+
+struct ParseErr;
+fn parse(line: &str) -> Result<TimeFreq, ParseErr> {
+    let spl: Vec<&str> = line.split(' ').collect();
+    if spl.len() < 2 {
+        Err(ParseErr)
+    } else {
+        let time_str = spl[0];
+        let freq_str = spl[1];
+        match (time_str.parse::<f32>(), freq_str.parse::<f32>()) {
+            (Ok(time), Ok(freq)) => Ok(TimeFreq { time, freq }),
+            _ => Err(ParseErr),
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
--- audio_light_show/src/main.rs
+++ audio_light_show/src/main.rs
@@ -2,12 +2,27 @@ extern crate env_logger;
 extern crate log;
 extern crate wiringpi;
 
+mod freq_file;
 mod music_pin;
 
 use log::info;
 
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const FILENAME: &str = "/tmp/bread.txt";
+
 fn main() {
     env_logger::init();
     info!("{}", VERSION);
+
+    let time_freqs = freq_file::load(FILENAME).expect("load");
+    for tf in time_freqs {
+        log::info!("{:?}", tf);
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct TimeFreq {
+    pub time: f32,
+    pub freq: f32,
 }
