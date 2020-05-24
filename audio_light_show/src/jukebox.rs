bffd19a6799567c6b9d68396ababbff84ab129a9
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-24 12:45:55 -0400

    Stay attached

--- audio_light_show/src/jukebox.rs
+++ audio_light_show/src/jukebox.rs
@@ -3,7 +3,5 @@ pub fn play(filename: &str) {
     let device = rodio::default_output_device().expect("device");
 
     let file = std::fs::File::open(filename).expect("file");
-    rodio::play_once(&device, BufReader::new(file))
-        .unwrap()
-        .detach();
+    rodio::play_once(&device, BufReader::new(file)).expect("play");
 }
