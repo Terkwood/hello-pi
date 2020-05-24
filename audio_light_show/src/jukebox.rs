1415e053fe46b7f73d36d12cc38bb6e5dc0a3627
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-24 12:43:41 -0400

    Add jukebox

---
+++ audio_light_show/src/jukebox.rs
@@ -0,0 +1,9 @@
+use std::io::BufReader;
+pub fn play(filename: &str) {
+    let device = rodio::default_output_device().expect("device");
+
+    let file = std::fs::File::open(filename).expect("file");
+    rodio::play_once(&device, BufReader::new(file))
+        .unwrap()
+        .detach();
+}
--- audio_light_show/src/main.rs
+++ audio_light_show/src/main.rs
@@ -1,24 +1,27 @@
 extern crate env_logger;
 extern crate log;
+extern crate rodio;
 extern crate wiringpi;
 
 mod aubiopitch;
+mod jukebox;
 mod music_pin;
 
 use log::info;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-const FILENAME: &str = "/tmp/bread.txt";
+const FREQ_FILE: &str = "/tmp/bread.txt";
+const MP3_FILE: &str = "/tmp/bread.mp3";
 
 fn main() {
     env_logger::init();
     info!("{}", VERSION);
 
-    let time_freqs = aubiopitch::parse_file(FILENAME).expect("parsed file");
-    for tf in time_freqs {
-        log::info!("{:?}", tf);
-    }
+    let time_freqs = aubiopitch::parse_file(FREQ_FILE).expect("parsed file");
+
+    // start playing the mp3
+    jukebox::play(MP3_FILE);
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
