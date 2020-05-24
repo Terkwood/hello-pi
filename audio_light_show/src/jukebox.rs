b0d404c4cf44313ec5da20616a719f471e67e200
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-24 12:47:55 -0400

    Use mp3

--- audio_light_show/src/jukebox.rs
+++ audio_light_show/src/jukebox.rs
@@ -1,7 +1,10 @@
 use std::io::BufReader;
 pub fn play(filename: &str) {
     let device = rodio::default_output_device().expect("device");
+    let sink = rodio::Sink::new(&device);
 
-    let file = std::fs::File::open(filename).expect("file");
-    rodio::play_once(&device, BufReader::new(file)).expect("play");
+    let file = std::fs::File::open(filename).expect("open");
+    sink.append(rodio::Decoder::new(BufReader::new(file)).expect("decoder"));
+
+    sink.sleep_until_end();
 }
