03f2d06fd64aefa1454afde9f311acc9cc06055b
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-24 15:02:51 -0400

    HACK

--- audio_light_show/src/jukebox.rs
+++ audio_light_show/src/jukebox.rs
@@ -62,7 +62,7 @@ pub fn blink_lights(time_freqs: Vec<TimeFreq>) {
 }
 
 const A4_TUNING_HZ: f32 = 440.0;
-const A4_KEY_POSITION: u16 = 48;
+const A4_KEY_POSITION: u16 = 68; // 48
 /// See https://en.m.wikipedia.org/wiki/Piano_key_frequencies
 fn freq_to_note(freq: f32) -> u16 {
     (39.86 * (freq / A4_TUNING_HZ).log10()) as u16 + A4_KEY_POSITION
@@ -78,14 +78,14 @@ mod tests {
     #[test]
     fn test_freq_to_note() {
         assert_eq!(A4_KEY_POSITION, freq_to_note(A4_TUNING_HZ));
-        assert_eq!(103, freq_to_note(5919.911)); // F#8
-        assert_eq!(26, freq_to_note(116.5409)); // Bb2
+        assert_eq!(102, freq_to_note(5919.911)); // F#8
+        assert_eq!(25, freq_to_note(116.5409)); // Bb2
     }
 
     #[test]
     fn test_twos_freq_to_note() {
         assert_eq!(A4_KEY_POSITION, twos_freq_to_note(A4_TUNING_HZ));
-        assert_eq!(103, twos_freq_to_note(5919.911)); // F#8
-        assert_eq!(26, twos_freq_to_note(116.5409)); // Bb2
+        assert_eq!(102, twos_freq_to_note(5919.911)); // F#8
+        assert_eq!(25, twos_freq_to_note(116.5409)); // Bb2
     }
 }
