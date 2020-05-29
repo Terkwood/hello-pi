4de50438132df8ea38eedcdddd600dfb3e08a5b3
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-29 10:47:45 -0400

    Stub out fetch

--- breadamp-ui/src/routes/home/index.js
+++ breadamp-ui/src/routes/home/index.js
@@ -1,11 +1,28 @@
 import { useState } from "preact/hooks";
 import "98.css";
 
+/// TODO TODO TODO
+/// TODO TODO TODO
+/// TODO TODO TODO
+/// TODO TODO TODO
+/// TODO TODO TODO
+/// TODO TODO TODO
+/// TODO TODO TODO
+const HOST = "http://192.168.1.100:3030";
+
+const post = async (command) => fetch(`${HOST}/${command}`);
+
 const Home = () => {
   const [msg, setMsg] = useState("stopped");
   const [isPlaying, setIsPlaying] = useState(false);
-  const prevTrack = () => setMsg("prev");
-  const nextTrack = () => setMsg("next");
+  const prevTrack = () => {
+    setMsg("prev");
+    return post("prev");
+  };
+  const nextTrack = () => {
+    setMsg("next");
+    return post("next");
+  };
   return (
     <div className="window-body">
       <p style={{ textAlign: "center" }}>{msg}</p>
@@ -15,6 +32,7 @@ const Home = () => {
           onClick={() => {
             setIsPlaying(!isPlaying);
             setMsg(!isPlaying ? "playing" : "stopped");
+            return post(!isPlaying ? "play" : "stop");
           }}
         >
           {isPlaying ? "⏹️" : "▶️"}
